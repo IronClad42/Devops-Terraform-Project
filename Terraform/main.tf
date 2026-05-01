91ec02d8ea7e607bd3eeced2d8be272f93382234
IronClad42 <krishnabhujbal176@gmail.com>
2026-05-01 16:03:54 +0530

    node js madhe change kele aahe me

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -515,6 +515,9 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       desired_size   = 1
+      min_size = 1
+      max_size = 3
+      
       instance_types = ["t3.micro"]
       ami_type = "AL2_x86_64"
     }
